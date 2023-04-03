84f59db24979589457a59a21195961c1b1fe2869
João Matias <123664411+joaomatias100@users.noreply.github.com>
2023-04-03 01:01:45 +0100

    Create RUN_THIS_FIRST.sql

---
+++ Ex5/RUN_THIS_FIRST.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE Ex5;
+
+USE Ex5;
+
+CREATE TABLE employee (
+	first_name VARCHAR(20),
+    last_name VARCHAR(20)
+);
+ 
+INSERT INTO employee VALUES("Kevin", "Spacey");
+INSERT INTO employee VALUES("Joe", "Ranger");
+ 
+CREATE TABLE customer (
+	f_name VARCHAR(20),
+    l_name VARCHAR(20)
+);
+ 
+INSERT INTO customer VALUES("Peter", "Thiel");
+INSERT INTO customer VALUES("Elon", "Musk");
