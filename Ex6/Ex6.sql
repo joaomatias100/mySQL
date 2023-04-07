a4872cfa713e6afa0ddf26c1befe7b6c2fe09ec0
João Matias <123664411+joaomatias100@users.noreply.github.com>
2023-04-07 17:02:46 +0100

    Update Ex6.sql

--- Ex6/Ex6.sql
+++ Ex6/Ex6.sql
@@ -2,19 +2,22 @@ CREATE DATABASE Ex6;
 
 USE Ex6;
 
-CREATE TABLE Student(
-	student_id int auto_increment,
-    university_id int not null,
-    PRIMARY KEY(student_id)
+CREATE TABLE Student (
+    student_id INT AUTO_INCREMENT,
+    university_id INT NOT NULL,
+    PRIMARY KEY (student_id)
 );
 
-CREATE TABLE University(
-	university_id int auto_increment,
-    university_name varchar(30) not null,
-	PRIMARY KEY(university_id)
+CREATE TABLE University (
+    university_id INT AUTO_INCREMENT,
+    university_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (university_id)
 );
 
-CREATE VIEW student_view AS(
-	SELECT s.student_id, u.university_name FROM Student s
-    INNER JOIN University u ON s.university_id = u.university_id
-);
+CREATE VIEW student_view AS
+    (SELECT 
+        s.student_id, u.university_name
+    FROM
+        Student s
+            INNER JOIN
+        University u ON s.university_id = u.university_id);
